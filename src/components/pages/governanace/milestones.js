a4c7944260b04811b281ab6254d4141ee3bfc700
Noman Fayyaz <0347241829zai>
2019-08-16 15:11:36 +0500

    Revert: style nfixing

--- src/components/pages/governanace/milestones.js
+++ src/components/pages/governanace/milestones.js
@@ -34,14 +34,12 @@ class Milestones extends Component {
             <GetMilestone data={this.props.milestone} />
               {this.state.createMilestone ? (
                 <CreateMilestone  id={this.props.milestone.length + 1}/>
-              ) : ''
-              // (
-              //   <div className="milestone_add">
-              //       <img src="./assets/img/plus.png" />
-              //       <p>Porpose New Milestone</p>
-              //   </div>
-              // )
-              }
+              ) : (
+                <div className="milestone_add">
+                    <img src="./assets/img/plus.png" />
+                    <p>Porpose New Milestone</p>
+                </div>
+              )}
           </section>
       
         )
