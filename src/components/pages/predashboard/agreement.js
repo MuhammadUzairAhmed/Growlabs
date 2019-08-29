e17b800959ecf369ab5669ad749e57fc25e10a87
Uzair <uzairahmed1494@gmail.com>
2019-08-29 18:37:09 +0500

    updated agreement(validation) and predahboard(validation) and project(dummy)  - 8-29-2019

--- src/components/pages/predashboard/agreement.js
+++ src/components/pages/predashboard/agreement.js
@@ -57,7 +57,7 @@ class Agreements extends Component {
     
     componentDidMount()
 	{
-		if(this.props.agreementData != null){
+		if(this.props.agreementData != ''){
       this.setState({
         x1:this.props.agreementData.x1,
         x2:this.props.agreementData.x2,
@@ -78,142 +78,46 @@ class Agreements extends Component {
   
 handleChange1 = (e) =>
 { this.setState({x1 : e.target.value},()=>{
-if (this.state.x1 == ''){this.setState({ x1Err: 'Please Fill this field' })}else{this.setState({ x1Err: '' })}})}
+if (this.state.x1 == ''){this.setState({ x1Err: 'Please Fill this field',x1Active:'' })}else{this.setState({ x1Err: '', x1Active:'active' })}})}
 handleChange2 = (e) =>
 { this.setState({x2 : e.target.value},()=>{
-if (this.state.x2 == ''){this.setState({ x2Err: 'Please Fill this field' })}else{this.setState({ x2Err: '' })}})}
+if (this.state.x2 == ''){this.setState({ x2Err: 'Please Fill this field',x2Active:'' })}else{this.setState({ x2Err: '', x2Active:'active' })}})}
 handleChange3 = (e) =>
 { this.setState({x3 : e.target.value},()=>{
-if (this.state.x3 == ''){this.setState({ x3Err: 'Please Fill this field' })}else{this.setState({ x3Err: '' })}})}
+if (this.state.x3 == ''){this.setState({ x3Err: 'Please Fill this field',x3Active:'' })}else{this.setState({ x3Err: '', x3Active:'active' })}})}
 handleChange4 = (e) =>
 { this.setState({x4 : e.target.value},()=>{
-if (this.state.x4 == ''){this.setState({ x4Err: 'Please Fill this field' })}else{this.setState({ x4Err: '' })}})}
+if (this.state.x4 == ''){this.setState({ x4Err: 'Please Fill this field',x4Active:'' })}else{this.setState({ x4Err: '', x4Active:'active' })}})}
 handleChange5 = (e) =>
 { this.setState({x5 : e.target.value},()=>{
-if (this.state.x5 == ''){this.setState({ x5Err: 'Please Fill this field' })}else{this.setState({ x5Err: '' })}})}
+if (this.state.x5 == ''){this.setState({ x5Err: 'Please Fill this field',x5Active:'' })}else{this.setState({ x5Err: '', x5Active:'active' })}})}
 handleChange6 = (e) =>
 { this.setState({x6 : e.target.value},()=>{
-if (this.state.x6 == ''){this.setState({ x6Err: 'Please Fill this field' })}else{this.setState({ x6Err: '' })}})}
+if (this.state.x6 == ''){this.setState({ x6Err: 'Please Fill this field',x6Active:'' })}else{this.setState({ x6Err: '', x6Active:'active' })}})}
 handleChange7 = (e) =>
 { this.setState({x7 : e.target.value},()=>{
-if (this.state.x7 == ''){this.setState({ x7Err: 'Please Fill this field' })}else{this.setState({ x7Err: '' })}})}
+if (this.state.x7 == ''){this.setState({ x7Err: 'Please Fill this field' ,x7Active:''})}else{this.setState({ x7Err: '', x7Active:'active' })}})}
 handleChange8 = (e) =>
 { this.setState({x8 : e.target.value},()=>{
-if (this.state.x8 == ''){this.setState({ x8Err: 'Please Fill this field' })}else{this.setState({ x8Err: '' })}})}
+if (this.state.x8 == ''){this.setState({ x8Err: 'Please Fill this field',x8Active:'' })}else{this.setState({ x8Err: '', x8Active:'active' })}})}
 handleChange9 = (e) =>
 { this.setState({x9 : e.target.value},()=>{
-if (this.state.x9 == ''){this.setState({ x9Err: 'Please Fill this field' })}else{this.setState({ x9Err: '' })}})}
+if (this.state.x9 == ''){this.setState({ x9Err: 'Please Fill this field',x9Active:'' })}else{this.setState({ x9Err: '', x9Active:'active' })}})}
 handleChange10 = (e) =>
 { this.setState({x10 : e.target.value},()=>{
-if (this.state.x10 == ''){this.setState({ x10Err: 'Please Fill this field' })}else{this.setState({ x10Err: '' })}})}
+if (this.state.x10 == ''){this.setState({ x10Err: 'Please Fill this field',x10Active:'' })}else{this.setState({ x10Err: '', x10Active:'active' })}})}
 handleChange11 = (e) =>
 { this.setState({x11 : e.target.value},()=>{
-if (this.state.x11 == ''){this.setState({ x11Err: 'Please Fill this field' })}else{this.setState({ x11Err: '' })}})}
+if (this.state.x11 == ''){this.setState({ x11Err: 'Please Fill this field',x11Active:'' })}else{this.setState({ x11Err: '', x11Active:'active' })}})}
 handleChange12 = (e) =>
 { this.setState({x12 : e.target.value},()=>{
-if (this.state.x12 == ''){this.setState({ x12Err: 'Please Fill this field' })}else{this.setState({ x12Err: '' })}})}
+if (this.state.x12 == ''){this.setState({ x12Err: 'Please Fill this field',x12Active:'' })}else{this.setState({ x12Err: '', x12Active:'active' })}})}
 handleChange13 = (e) =>
 { this.setState({x13 : e.target.value},()=>{
-  if (this.state.x13 == ''){this.setState({ x13Err: 'Please Fill this field' })}else{this.setState({ x13Err: '' })}})}
+  if (this.state.x13 == ''){this.setState({ x13Err: 'Please Fill this field',x13Active:'' })}else{this.setState({ x13Err: '', x13Active:'active' })}})}
 
 
-  handleChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value
-    }, () => {
-      if (this.state.x1 == '') {
-        this.setState({ x1Err: 'Please Fill this field', x1Active:'' })
-      } else {
-        this.setState({ x1Err: '', x1Active:'active' })
-      }
-      if (this.state.x2 == '') {
-        this.setState({ x2Err: 'Please Fill this field' , x2Active:''})
-      } else {
-        this.setState({ x2Err: '', x2Active:'active' })
-      }
-      if (this.state.x3 == '') {
-        this.setState({ x3Err: 'Please Fill this field', x3Active:'' })
-      } else {
-        this.setState({ x3Err: '', x3Active:'active' })
-      }
-      if (this.state.x4 == '') {
-        this.setState({ x4Err: 'Please Fill this field' , x4Active:''})
-      } else {
-        this.setState({ x4Err: '', x4Active:'active' })
-      }
-      if (this.state.x5 == '') {
-        this.setState({ x5Err: 'Please Fill this field', x5Active:'' })
-      } else {
-        this.setState({ x5Err: '', x5Active:'active' })
-      }
-      if (this.state.x6 == '') {
-        this.setState({ x6Err: 'Please Fill this field', x6Active:'' })
-      } else {
-        this.setState({ x6Err: '', x6Active:'active' })
-      }
-      if (this.state.x7 == '') {
-        this.setState({ x7Err: 'Please Fill this field' , x7Active:''})
-      } else {
-        this.setState({ x7Err: '', x7Active:'active' })
-      }
-      if (this.state.x8 == '') {
-        this.setState({ x8Err: 'Please Fill this field', x8Active:'' })
-      } else {
-        this.setState({ x8Err: '', x8Active:'active' })
-      }
-      if (this.state.x9 == '') {
-        this.setState({ x9Err: 'Please Fill this field', x9Active:'' })
-      } else {
-        this.setState({ x9Err: '', x9Active:'active' })
-      }
-      if (this.state.x10 == '') {
-        this.setState({ x10Err: 'Please Fill this field' , x10Active:''})
-      } else {
-        this.setState({ x10Err: '', x10Active:'active' })
-      }
-      if (this.state.x11 == '') {
-        this.setState({ x11Err: 'Please Fill this field' , x11Active:''})
-      } else {
-        this.setState({ x11Err: '', x11Active:'active' })
-      }
-      if (this.state.x12 == '') {
-        this.setState({ x12Err: 'Please Fill this field' , x12Active:''})
-      } else {
-        this.setState({ x12Err: '', x12Active:'active' })
-      }
-      if (this.state.x13 == '') {
-        this.setState({ x13Err: 'Please Fill this field', x13Active:'' })
-      } else {
-        this.setState({ x13Err: '', x13Active:'active' })
-      }
-
-       values = {
-        x1: this.state.x1,
-        x2: this.state.x2,
-        x3: this.state.x3,
-        x4: this.state.x4,
-        x5: this.state.x5,
-        x6: this.state.x6,
-        x7: this.state.x7,
-        x8: this.state.x8,
-        x9: this.state.x9,
-        x10: this.state.x10,
-        x11: this.state.x11,
-        x12: this.state.x12,
-        x13: this.state.x13
-      }
-      const {x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13} = this.state;
-      if(x1 != '' && x2 != '' && x3 != '' && x4 != '' && x5 != '' && x6 != ''  && x7 != '' && x8 != '' && x9 != ''
-      && x10 != '' && x11 != '' && x12 != '' && x13 != '' )
-      {
-        this.setState({colorButn: '#19D192'})
-      }else
-      {
-        this.setState({colorButn: '#d4d9dd'})
-      }
-    })
-
-  }
+ 
   handleAccept =()=>{
     
     const {x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13} = this.state;
@@ -259,15 +163,15 @@ handleChange13 = (e) =>
             <h1>Financial</h1>
             <h4>BONUS AMOUNT FOR ACHIEVING DEADLINE</h4>
             <div className="input_box">
-              <input type="text" name="x1" className={this.state.x1Active} placeholder="€0" value={this.state.x1} onChange={this.handleChange} />
-              <span className="FildEror">{this.state.x1Err}</span>
+              <input type="text" name="x1" className={this.state.x1Active} placeholder="€0" value={this.state.x1} onChange={this.handleChange1} />
+              {/* <span className="FildEror">{this.state.x1Err}</span> */}
 
               <div class="tooltip"><img src="./assets/img/1024px-Infobox_info_icon.svg Copy 4.png" class="" /><span class="tooltiptext">Tooltip text</span></div>
             </div>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley </p>
             <h4>BONUS AMOUNT FOR ACHIEVING DEADLINE</h4>
-            <div className="input_box"><input type="text" name="x2" className={this.state.x2Active} placeholder="Minimally 10 days before start new milestone" value={this.state.x2} onChange={this.handleChange} />
-              <span className="FildEror">{this.state.x2Err}</span>
+            <div className="input_box"><input type="text" name="x2" className={this.state.x2Active} placeholder="Minimally 10 days before start new milestone" value={this.state.x2} onChange={this.handleChange2} />
+              {/* <span className="FildEror">{this.state.x2Err}</span> */}
               <div class="tooltip"><img src="./assets/img/1024px-Infobox_info_icon.svg Copy 4.png" class="" /><span class="tooltiptext">Tooltip text</span></div>
             </div>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley </p>
@@ -277,19 +181,19 @@ handleChange13 = (e) =>
 
 
               <input type="text" name="x3" placeholder="Monday"  className={this.state.x3Active} value={this.state.x3} onChange={this.handleChange3} />
-              <span style={{ color: 'red' }}>{this.state.x3Err}</span>
+              {/* <span style={{ color: 'red' }}>{this.state.x3Err}</span> */}
 
               <div class="tooltip"><img src="./assets/img/1024px-Infobox_info_icon.svg Copy 4.png" class="" /><span class="tooltiptext">Tooltip text</span></div>
             </div>
             <h4>SPRINTS CALLS</h4>
             <div className="input_box hlf">
 
-              <input type="text" name="x4" className={this.state.x4Active} placeholder="WED, Fri" value={this.state.x4} onChange={this.handleChange} />
+              <input type="text" name="x4" className={this.state.x4Active} placeholder="WED, Fri" value={this.state.x4} onChange={this.handleChange4} />
               <span className="FildEror">{this.state.x4Err}</span>
             </div>
             <div className="input_box hlf rgt">
-              <input type="text" name="x5" className={this.state.x5Active} placeholder="9:00 AM (EPT)" value={this.state.x5} onChange={this.handleChange} />
-              <span className="FildEror">{this.state.x5Err}</span>
+              <input type="text" name="x5" className={this.state.x5Active} placeholder="9:00 AM (EPT)" value={this.state.x5} onChange={this.handleChange5} />
+              {/* <span className="FildEror">{this.state.x5Err}</span> */}
 
 
             </div>
@@ -299,7 +203,7 @@ handleChange13 = (e) =>
 
          
               <input type="text" name="x6" placeholder="Skype"  className={this.state.x6Active} value={this.state.x6} onChange={this.handleChange6} />
-              <span style={{ color: 'red' }}>{this.state.x6Err}</span>
+              {/* <span style={{ color: 'red' }}>{this.state.x6Err}</span> */}
 
               <div class="tooltip"><img src="./assets/img/1024px-Infobox_info_icon.svg Copy 4.png" class="" /><span class="tooltiptext">Tooltip text</span></div>
             </div>
@@ -331,7 +235,7 @@ handleChange13 = (e) =>
 
 
               <input type="text" name="x9" placeholder="Yes" value={this.state.x9} className={this.state.x9Active} onChange={this.handleChange9} />
-              <span style={{ color: 'red' }}>{this.state.x9Err}</span>
+              {/* <span style={{ color: 'red' }}>{this.state.x9Err}</span> */}
 
               <div class="tooltip"><img src="./assets/img/1024px-Infobox_info_icon.svg Copy 4.png" class="" /><span class="tooltiptext">Tooltip text</span></div>
             </div>
@@ -342,7 +246,7 @@ handleChange13 = (e) =>
               
 
               <input type="text" name="x10" placeholder="3 Days in Advance" className={this.state.x10Active} value={this.state.x10} onChange={this.handleChange10} />
-              <span style={{ color: 'red' }}>{this.state.x10Err}</span>
+              {/* <span style={{ color: 'red' }}>{this.state.x10Err}</span> */}
 
               <div class="tooltip"><img src="./assets/img/1024px-Infobox_info_icon.svg Copy 4.png" class="" /><span class="tooltiptext">Tooltip text</span></div>
             </div>
@@ -352,7 +256,7 @@ handleChange13 = (e) =>
               
 
               <input type="text" name="x11" placeholder="1 Sprint in advance" className={this.state.x11Active} value={this.state.x11} onChange={this.handleChange11} />
-              <span style={{ color: 'red' }}>{this.state.x11Err}</span>
+              {/* <span style={{ color: 'red' }}>{this.state.x11Err}</span> */}
 
               <div class="tooltip"><img src="./assets/img/1024px-Infobox_info_icon.svg Copy 4.png" class="" /><span class="tooltiptext">Tooltip text</span></div>
             </div>
@@ -362,7 +266,7 @@ handleChange13 = (e) =>
              
 
               <input type="text" name="x12" placeholder="1 Sprint in advance" className={this.state.x12Active} value={this.state.x12} onChange={this.handleChange12} />
-              <span style={{ color: 'red' }}>{this.state.x12Err}</span>
+              {/* <span style={{ color: 'red' }}>{this.state.x12Err}</span> */}
 
               <div class="tooltip"><img src="./assets/img/1024px-Infobox_info_icon.svg Copy 4.png" class="" /><span class="tooltiptext">Tooltip text</span></div>
             </div>
@@ -373,7 +277,7 @@ handleChange13 = (e) =>
               
 
               <input type="text" name="x13" placeholder="Yes" value={this.state.x13} className={this.state.x13Active} onChange={this.handleChange13} />
-              <span style={{ color: 'red' }}>{this.state.x13Err}</span>
+              {/* <span style={{ color: 'red' }}>{this.state.x13Err}</span> */}
 
               <div class="tooltip"><img src="./assets/img/1024px-Infobox_info_icon.svg Copy 4.png" class="" /><span class="tooltiptext">Tooltip text</span></div>
             </div>
